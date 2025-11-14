0da127bd2840cbcb84f1abe80c1a170356c248ef
uv8904 <uvjangra2@gmail.com>
2025-11-14 21:23:16 +0530

    Fix password casing in MongoDB URI
    Corrected the password casing in the DATABASE_URI.

--- info.py
+++ info.py
@@ -73,7 +73,7 @@ STAR_PREMIUM_PLANS = {
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Yuvi1234:yuvi1234>@cluster0.fv4jwrn.mongodb.net/?appName=Cluster0")  # MongoDB URI for the database
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Yuvi1234:Yuvi1234>@cluster0.fv4jwrn.mongodb.net/?appName=Cluster0")  # MongoDB URI for the database
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0") # Database name (default: cluster)
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'dreamcinezone_files') # Collection name (default: dreamcinezone_files)
 
